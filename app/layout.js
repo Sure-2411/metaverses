b6aa59c529b87cb2f7ac6ff9b224ff2355157cb7
Sure-2411 <112511352+Sure-2411@users.noreply.github.com>
2023-08-31 03:45:10 +0530

    Add files via upload

---
+++ app/layout.js
@@ -0,0 +1,13 @@
+import '../styles/globals.css';
+
+const RootLayout = ({ children }) => (
+  <html lang="en">
+    <head>
+      <link rel="preconnect" href="https://www.linkedin.com/in/suresh-kumar1968/" />
+      <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
+    </head>
+    <body>{children}</body>
+  </html>
+);
+
+export default RootLayout;
